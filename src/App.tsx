340c1d69dbf2f1870dc46bd3f3a908de3a00a128
Jordan Acock <jpacock@us.ibm.com>
2024-05-10 09:11:59 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Hub App</h1>
+      <ul>
+        <li><a href="/sprinkler">App 2</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App
