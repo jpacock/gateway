a8fb2e93315b82cff4e06c4939e211bc448bad0b
Jordan Acock <jpacock@us.ibm.com>
2024-05-10 10:17:06 -0500

    Setup daisy ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+
 import './App.css'
+import './styles.css';
+import 'daisyui/dist/full.css';
 
 function App() {
   return (
     <div>
-      <h1>Welcome to My Hub App</h1>
-      <ul>
-        <li><a href="/sprinkler">App 2</a></li>
-      </ul>
+      <h1 className="mb-5">Gateway</h1>
+      <div className="flex flex-col space-y-5">
+        <a href="https://jpacock.com/sprinkler">
+          <button className="btn btn-outline btn-info">Sprinkler</button>
+        </a>
+        <a href="https://jpacock.com/recipes">
+          <button className="btn btn-outline btn-info">Recipes</button>
+        </a>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.jsx',
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+}
